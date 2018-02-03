a1b68f64896ecc2e0d27bf306def01a2b9b9aaa0
Jakob Schindegger <jakob@fakob.com>
2018-02-03 12:17:24 +0100

    added react-draggable package

--- app/components/VideoPlayer.js
+++ app/components/VideoPlayer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Draggable from 'react-draggable';
 import styles from './VideoPlayer.css';
 
 const pathModule = require('path');
@@ -53,6 +54,21 @@ class VideoPlayer extends Component {
         </div>
         <div className={`${styles.controlsWrapper}`}>
           <div className={`${styles.timelineWrapper}`}>
+            <Draggable
+              axis="x"
+              handle=".handle"
+              defaultPosition={{ x: 0, y: 0 }}
+              position={null}
+              grid={[25, 25]}
+              onStart={this.handleStart}
+              onDrag={this.handleDrag}
+              onStop={this.handleStop}
+            >
+              <div>
+                <div className="handle">Drag from here</div>
+                <div className={`${styles.currentTime}`} style={{ left: `${((this.state.currentTime || 0) / (this.state.duration || 1)) * 100}%` }} />
+              </div>
+            </Draggable>
             <div className={`${styles.currentTime}`} style={{ left: `${((this.state.currentTime || 0) / (this.state.duration || 1)) * 100}%` }} />
             <div
               className={`${styles.cutStartTime}`}
