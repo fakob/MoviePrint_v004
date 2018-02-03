a59e894bf27ba6054c3c33334a64d39aa4b5b57f
Jakob Schindegger <jakob@fakob.com>
2018-02-03 11:53:50 +0100

    video player now shows controls

--- app/components/VideoPlayer.js
+++ app/components/VideoPlayer.js
@@ -4,36 +4,70 @@ import styles from './VideoPlayer.css';
 
 const pathModule = require('path');
 
-const VideoPlayer = ({ path, onPlay, onPause }) => {
-  return (
-    <div>
-      <div id="player">
-        <video
-          controls
-          src={`${pathModule.dirname(path)}/${encodeURIComponent(pathModule.basename(path))}` || ''}
-          onPlay={onPlay}
-          onPause={onPause}
-          autoPlay
-          // onRateChange={() => this.playbackRateChange()}
-          // onDurationChange={e => this.onDurationChange(e.target.duration)}
-          // onTimeUpdate={e => this.setState({ currentTime: e.target.currentTime })}
-        />
-      </div>
-      <div className={`${styles.controlsWrapper}`}>
-        <div className={`${styles.timelineWrapper}`}>
-          <div className={`${styles.currentTime}`} style={{ left: `${((0) / (1)) * 100}%` }} />
-          <div
-            className={`${styles.cutStartTime}`}
-            style={{
-              left: `${((0) / (1)) * 100}%`,
-              width: `${(((0) - (0)) / (1)) * 100}%`,
-            }}
-          />
+class VideoPlayer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // working: false,
+      // filePath: '', // Setting video src="" prevents memory leak in chromium
+      // playing: false,
+      currentTime: undefined,
+      duration: undefined,
+      // cutStartTime: 0,
+      // cutEndTime: undefined,
+      // fileFormat: undefined,
+    };
+
+    // const resetState = () => {
+    //   const video = getVideo();
+    //   video.currentTime = 0;
+    //   video.playbackRate = 1;
+    //   this.setState(defaultState);
+    // };
+  }
+
+  onDurationChange(duration) {
+    this.setState({ duration });
+    // if (!this.state.cutEndTime) this.setState({ cutEndTime: duration });
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="player">
+          <video
+            controls
+            src={`${pathModule.dirname(this.props.path)}/${encodeURIComponent(pathModule.basename(this.props.path))}` || ''}
+            width="640px"
+            height="360px"
+            // onPlay={onPlay}
+            // onPause={onPause}
+            autoPlay
+            // onRateChange={() => this.playbackRateChange()}
+            onDurationChange={e => this.onDurationChange(e.target.duration)}
+            onTimeUpdate={e => this.setState({ currentTime: e.target.currentTime })}
+          >
+            <track kind="captions" />
+          </video>
+        </div>
+        <div className={`${styles.controlsWrapper}`}>
+          <div className={`${styles.timelineWrapper}`}>
+            <div className={`${styles.currentTime}`} style={{ left: `${((this.state.currentTime || 0) / (this.state.duration || 1)) * 100}%` }} />
+            <div
+              className={`${styles.cutStartTime}`}
+              style={{
+                left: `${((0) / (1)) * 100}%`,
+                width: `${(((0) - (0)) / (1)) * 100}%`,
+              }}
+            />
+            <div id="currentTimeDisplay">{this.state.currentTime}</div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 VideoPlayer.contextTypes = {
   store: PropTypes.object
