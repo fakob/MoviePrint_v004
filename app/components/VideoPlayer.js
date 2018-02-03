97ea857c596aac2c8cf6e56bacacdfa5e0e28876
Jakob Schindegger <jakob@fakob.com>
2018-02-03 19:46:57 +0100

    changed windowWidth to videoWidth

--- app/components/VideoPlayer.js
+++ app/components/VideoPlayer.js
@@ -13,29 +13,17 @@ class VideoPlayer extends Component {
       // working: false,
       // filePath: '', // Setting video src="" prevents memory leak in chromium
       // playing: false,
-      windowWidth: 0,
       currentTime: undefined,
       duration: undefined,
       controlledPosition: {
         x: 0, y: 0
       },
+      width: 640,
+      height: 360
       // cutStartTime: 0,
       // cutEndTime: undefined,
       // fileFormat: undefined,
     };
-  }
-
-  componentDidMount() {
-    this.updateWindowDimensions();
-    window.addEventListener('resize', this.updateWindowDimensions);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateWindowDimensions);
-  }
-
-  updateWindowDimensions() {
-    this.setState({ windowWidth: window.innerWidth });
   }
 
   onDurationChange(duration) {
@@ -45,22 +33,20 @@ class VideoPlayer extends Component {
 
   onTimeUpdate(currentTime) {
     this.setState({ currentTime });
-    const newX = (currentTime / this.state.duration) * this.state.windowWidth;
+    const newX = (currentTime / this.state.duration) * this.state.width;
     this.setState({ controlledPosition: { x: newX, y: 0 } });
   }
 
   onControlledDrag(e, position) {
     const { x } = position;
     this.setState({ controlledPosition: { x, y: 0 } });
-    const newCurrentTime = ((x * 1.0) / this.state.windowWidth) * this.state.duration;
-    // console.log(`${x} : ${newCurrentTime} : ${this.state.duration} : ${this.state.windowWidth}`);
+    const newCurrentTime = ((x * 1.0) / this.state.width) * this.state.duration;
     this.video.currentTime = newCurrentTime;
     this.setState({ currentTime: newCurrentTime });
   }
 
   render() {
     this.onControlledDrag = this.onControlledDrag.bind(this);
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
 
     const dragHandlers = { onStart: this.onStart, onStop: this.onStop };
     const { controlledPosition } = this.state;
@@ -74,8 +60,8 @@ class VideoPlayer extends Component {
             controls
             muted
             src={`${pathModule.dirname(this.props.path)}/${encodeURIComponent(pathModule.basename(this.props.path))}` || ''}
-            width="640px"
-            height="360px"
+            width={this.state.width}
+            height={this.state.height}
             onDurationChange={e => this.onDurationChange(e.target.duration)}
             onTimeUpdate={e => this.onTimeUpdate(e.target.currentTime)}
           >
@@ -88,8 +74,8 @@ class VideoPlayer extends Component {
               axis="x"
               handle=".handle"
               position={controlledPosition}
-              {...dragHandlers}
               onDrag={this.onControlledDrag}
+              {...dragHandlers}
             >
               <div>
                 <div className={`${styles.currentTime} handle`} />
