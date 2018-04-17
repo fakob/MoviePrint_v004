31de83b660e1f8913cf466fc180e36bd1bc632a9
Jakob Schindegger <>
2018-04-17 09:19:39 +0200

    changed Math.pow to exponentiation operator

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -132,7 +132,7 @@ export const formatBytes = (bytes, decimals) => {
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
   // return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+  return `${parseFloat((bytes / (k ** i)).toFixed(dm))} ${sizes[i]}`;
 };
 
 export const saveBlob = (blob, fileName) => {
