865b66242fb0a1c60065ff4e7799300cd4c32212
Jakob Schindegger <>
2018-04-24 15:50:54 +0200

    adding production switch for dexie to speedup production

--- app/utils/db.js
+++ app/utils/db.js
@@ -2,7 +2,11 @@ import Dexie from 'dexie';
 import FileObject from './fileObject';
 
 // Force debug mode to get async stacks from exceptions.
-Dexie.debug = true; // In production, set to false to increase performance a little.
+if (process.env.NODE_ENV === 'production') {
+  Dexie.debug = false;
+} else {
+  Dexie.debug = true; // In production, set to false to increase performance a little.
+}
 const imageDB = new Dexie('ImageDatabase');
 imageDB.version(1).stores({
   frameList: '&frameId, fileId, frameNumber, isPosterFrame, [fileId+frameNumber]'
