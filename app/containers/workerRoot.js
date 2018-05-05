46b0b2bfc5102bb1a772476a9bce0525e9ba41f4
Jakob Schindegger <jakob@fakob.com>
2018-05-05 13:37:42 +0200

    some restructuring

--- app/containers/workerRoot.js
+++ app/containers/workerRoot.js
@@ -1,155 +1,17 @@
 // @flow
-import React, { Component } from 'react';
-import { Provider, connect } from 'react-redux';
-import '../app.global.css';
-import styles from './App.css';
-import SortedVisibleThumbGrid from '../containers/VisibleThumbGrid';
-import { getVisibleThumbs, getScaleValueObject, getMoviePrintColor, getColumnCount } from '../utils/utils';
-import saveMoviePrint from '../utils/saveMoviePrint';
+import React from 'react';
+import { Provider } from 'react-redux';
+import WorkerApp from './WorkerApp';
 
-const { ipcRenderer } = require('electron');
-
-// export default function WorkerRoot({ store }: RootType) {
-class WorkerRoot extends Component {
-  constructor() {
-    super();
-    this.state = {
-      savingMoviePrint: false,
-      data: {}
-    };
-
-    // this.onSaveMoviePrint = this.onSaveMoviePrint.bind(this);
-  }
-
-
-  componentDidMount() {
-    ipcRenderer.on('action-saved-MoviePrint-done', (event) => {
-      this.setState({
-        savingMoviePrint: false
-      });
-    });
-
-    ipcRenderer.on('action-save-MoviePrint', (event, data) => {
-      console.log(data.file);
-      this.setState({
-        savingMoviePrint: true,
-        data
-      });
-    });
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (!prevState.savingMoviePrint && this.state.savingMoviePrint) {
-      console.log(this.state.data.file);
-      saveMoviePrint(this.state.data.elementId, this.state.data.exportPath, this.state.data.file, this.state.data.scale, this.state.data.outputFormat, this.state.data.overwrite, this.state.data.saveIndividualThumbs, this.state.data.thumbs);
-    }
-  }
-
-  // onSaveMoviePrint() {
-  //   this.setState(
-  //     { savingMoviePrint: true },
-  //     ipcRenderer.send('send-get-poster-frame', fileId, filePath, posterFrameId, lastItem)
-  //     saveMoviePrint(
-  //       'ThumbGrid',
-  //       this.props.settings.defaultOutputPath,
-  //       this.props.file,
-  //       this.props.settings.defaultThumbnailScale / this.state.outputScaleCompensator,
-  //       this.props.settings.defaultOutputFormat,
-  //       this.props.settings.defaultSaveOptionOverwrite,
-  //       this.props.settings.defaultSaveOptionIncludeIndividual,
-  //       this.props.thumbs
-  //     )
-  //   );
-  // }
-
-  render() {
-    const { store } = this.context;
-    const state = store.getState();
-    return (
-      <Provider store={this.props.store}>
-        {this.state.savingMoviePrint &&
-          <div
-            ref={(r) => { this.divOfSortedVisibleThumbGridRef = r; }}
-            className={`${styles.ItemMain}`}
-            style={{
-              width: `${getScaleValueObject(
-                this.state.data.file,
-                state.undoGroup.present.settings,
-                this.state.data.file.columnCount, this.state.data.file.thumbCount,
-                1360, 800, // values not needed for saveMoviePrint
-                state.visibilitySettings.showMoviePrintView,
-                1,
-                true
-              ).newMoviePrintWidth}px`
-            }}
-          >
-            <SortedVisibleThumbGrid
-              inputRef={(r) => { this.sortedVisibleThumbGridRef = r; }}
-              showSettings={false}
-              file={this.state.data.file}
-              selectedThumbId={undefined}
-
-              colorArray={getMoviePrintColor(state.undoGroup.present.settings.defaultThumbCountMax)}
-              thumbCount={this.state.data.file.thumbCount}
-
-              showMoviePrintView
-              scaleValueObject={getScaleValueObject(
-                this.state.data.file,
-                state.undoGroup.present.settings,
-                this.state.data.file.columnCount, this.state.data.file.thumbCount,
-                1360, 800, // values not needed for saveMoviePrint
-                state.visibilitySettings.showMoviePrintView,
-                1,
-                true
-              )}
-              keyObject={{}}
-            />
-          </div>
-        }
-      </Provider>
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  // const tempCurrentFileId = state.undoGroup.present.settings.currentFileId;
-  // const tempThumbs = (state.undoGroup.present
-  //   .thumbsByFileId[tempCurrentFileId] === undefined)
-  //   ? undefined : state.undoGroup.present
-  //     .thumbsByFileId[tempCurrentFileId].thumbs;
-  return {
-    // thumbs: getVisibleThumbs(
-    //   tempThumbs,
-    //   state.visibilitySettings.visibilityFilter
-    // ),
-    // currentFileId: tempCurrentFileId,
-    // files: state.undoGroup.present.files,
-    // file: state.undoGroup.present.files
-    //   .find((file) => file.id === tempCurrentFileId),
-    // settings: state.undoGroup.present.settings,
-    // visibilitySettings: state.visibilitySettings,
-    // thumbCount: getColumnCount(
-    //   state.undoGroup.present.files.find((file) => file.id === tempCurrentFileId),
-    //   state.undoGroup.present.settings
-    // ),
-    // columnCount: getThumbsCount(
-    //   state.undoGroup.present.files.find((file) => file.id === tempCurrentFileId),
-    //   this.props.thumbsByFileId,
-    //   state.undoGroup.present.settings,
-    //   state.visibilitySettings.visibilityFilter
-    // ),
-    // thumbsByFileId: state.undoGroup.present.thumbsByFileId,
-    // colorArray: getMoviePrintColor(state.undoGroup.present.settings.defaultThumbCountMax),
-    // scaleValueObject: getScaleValueObject(
-    //   state.undoGroup.present.files.find((file) => file.id === tempCurrentFileId),
-    //   state.undoGroup.present.settings,
-    //   state.undoGroup.present.files.find((file) => file.id === tempCurrentFileId).columnCount, state.undoGroup.present.files.find((file) => file.id === tempCurrentFileId).thumbCount,
-    //   1360, 800, // values not needed for saveMoviePrint
-    //   state.visibilitySettings.showMoviePrintView,
-    //   1,
-    //   true
-    // )
-  };
+type RootType = {
+  store: {},
+  history: {}
 };
 
-export default connect(mapStateToProps)(WorkerRoot);
+export default function WorkerRoot({ store, history }: RootType) {
+  return (
+    <Provider store={store}>
+      <WorkerApp history={history} />
+    </Provider>
+  );
+}
--- app/worker.js
+++ app/worker.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import { ActionCreators as UndoActionCreators } from 'redux-undo';
-import WorkerRoot from './containers/workerRoot';
+import WorkerRoot from './containers/WorkerRoot';
 import { configureStore, history } from './store/configureStore';
 
 const store = configureStore();
@@ -15,10 +15,11 @@ render(
 );
 
 if (module.hot) {
-  module.hot.accept('./containers/workerRoot', () => {
+  module.hot.accept('./containers/WorkerRoot', () => {
+    const NextWorkerRoot = require('./containers/WorkerRoot').default; // eslint-disable-line global-require
     render(
       <AppContainer>
-        <WorkerRoot store={store} history={history} />
+        <NextWorkerRoot store={store} history={history} />
       </AppContainer>,
       document.getElementById('worker')
     );
