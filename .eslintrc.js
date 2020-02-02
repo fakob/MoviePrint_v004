e6d0372c12d18b0f21fac6f4064fbeb7211899f6
Jakob Schindegger <jakob@fakob.com>
2020-02-02 10:51:17 +0100

    Fixing eslint import resolver

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
   extends: 'erb',
   settings: {
-    'import/resolver': {
-      webpack: {
-        config: require.resolve('./configs/webpack.config.eslint.js')
-      }
-    }
+    'import/resolver': 'webpack'
   }
 }
